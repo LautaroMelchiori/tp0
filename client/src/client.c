d50013bb3f4d5ef17dd16e2fdff4ddcc026b287b
Lautaro Melchiori <lautaromelchiori01@gmail.com>
2024-04-02 19:42:59 +0000

    Agrego el config destroy que me olvide

--- client/src/client.c
+++ client/src/client.c
@@ -111,4 +111,5 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 	
 	log_destroy(logger);
+	config_destroy(config);
 }
